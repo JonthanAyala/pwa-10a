dd7929f9edd148fc2de1b67a791e13f974012020
JonthanAyala <20223tn085@utez.edu.mx>
2025-10-20 17:17:54 -0600

    [FIX] register.js

--- register.js
+++ register.js
@@ -7,17 +7,13 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         // Registra el Service Worker en la raíz (/) del sitio.
-        navigator.serviceWorker
-          .register("https://jonthanayala.github.io/pwa-10a/sw.js")
-          .then((registration) => {
-            console.log(
-              "Service Worker registrado con éxito. Scope:",
-              registration.scope
-            );
-          })
-          .catch((error) => {
-            console.error("Fallo en el registro del Service Worker:", error);
-          });
+        navigator.serviceWorker.register('./sw.js')
+            .then(registration => {
+                console.log('Service Worker registrado con éxito. Scope:', registration.scope);
+            })
+            .catch(error => {
+                console.error('Fallo en el registro del Service Worker:', error);
+            });
     });
 } else {
     console.warn('El navegador no soporta Service Workers.');
